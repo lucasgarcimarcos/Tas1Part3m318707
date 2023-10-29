bf70966181448c6941d496625a2978c98a68eebb
Lololeifo <100757814+Lololeifo@users.noreply.github.com>
2023-10-29 16:05:26 +0100

    Path1

--- Assets/FollowWP2.cs
+++ Assets/FollowWP2.cs
@@ -6,7 +6,7 @@ public class FollowWP2 : MonoBehaviour
 {
     Transform goal;
     float speed = 5.0f;
-    float accuracy = 1.0f;
+    float accuracy = 5.0f;
     float rotSpeed = 2.0f;
 
     public GameObject wpManager;
@@ -20,6 +20,8 @@ public class FollowWP2 : MonoBehaviour
         wps = wpManager.GetComponent<WPManager>().waypoint;
         g = wpManager.GetComponent<WPManager>().graph;
         currentNode = wps[0];
+
+        Invoke("GoToRuin", 2);
     }
 
     public void GoToHeli()
@@ -35,8 +37,21 @@ public class FollowWP2 : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        if(g.pathList.Count == 0 || currentWP == g.pathList.Count) { return; }
+        if (Vector3.Distance(g.pathList[currentWP].getId().transform.position, this.transform.position) < accuracy) 
+        {
+            currentNode = g.pathList[currentWP].getId();
+            currentWP++;
+        }
+        if(currentWP < g.pathList.Count)
+        {
+            goal = g.pathList[currentWP].getId().transform;
+            Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);
+            Vector3 direction = lookAtGoal - this.transform.position;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
+            this.transform.Translate(0, 0, speed * Time.deltaTime);
+        }
     }
 }
--- Assets/Graphs/Graph.cs
+++ Assets/Graphs/Graph.cs
@@ -6,7 +6,7 @@ public class Graph
 {
    List<Edge> edges = new List<Edge>();
    List<Node> nodes = new List<Node>();    
-   List<Node> pathList = new List<Node>();
+   public List<Node> pathList = new List<Node>();
 
     public Graph()
     {
